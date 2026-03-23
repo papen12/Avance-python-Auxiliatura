c985be271d86e15060d95702ed5062208dd0300b
papen12 <papenfabri2.0@gmail.com>
2026-03-23 18:22:02 -0400

    Operadores Aritmeticos clase python 23/03/2026

---
+++ Operadores.py
@@ -0,0 +1,44 @@
+from math import sqrt
+num1=5
+num2=10
+print("Mi numero es: ",num1)
+num1="hola"
+print("Mi numero es: ",num1)
+# suma=num1+num2
+
+# print("La suma es: ",suma)
+# print(num1,num2)
+
+num3=10
+num4=2
+
+multiplicacion=num3*num4
+print("El resultado de la multiplicacion es: ",multiplicacion)
+
+
+num5=60
+num6=6
+
+division=60/6
+print("El resultado de la division es: ",multiplicacion)
+
+
+num7=8
+num8=2
+modular=num7%num8
+print("El resultado de la division modular es: ",modular)
+
+num9=55
+num10=2
+entera=55//2
+print("El resultado de la division entera es: ",entera)
+
+num11=2
+num12=5
+
+potencia=num11**num12
+print("El resultado de la potenciacion es: ",potencia)
+
+raiz=16**(1/2)
+print("El resultado de la raiz es: ",raiz)
+
